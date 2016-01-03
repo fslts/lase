1dfd8edd0a05ae494723b23878658dd9807e9bf6
Filip Soltes <fillo@chrn.sk>
2016-01-03 23:02:21 +0100

    encoding for python2

--- lase.py
+++ lase.py
@@ -1,7 +1,13 @@
+import sys
+
 import config
 import src.scanner as scanner
 
 def main():
+
+    reload(sys)
+    sys.setdefaultencoding("utf-8")
+
     scanner.scan(config.ranges)
 
 if __name__ == '__main__':
