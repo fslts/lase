2e521c18d35931ea1aadb5c73a200251e28ddb95
Filip Soltes <fillo@chrn.sk>
2016-01-07 19:08:15 +0100

    host state from cache tweaked

--- src/utils/cache.py
+++ src/utils/cache.py
@@ -1,17 +1,23 @@
 import redis
 
 #TODO to config
-HOST_PREFIX = 'lase.host.'
-HOST_EXPIRE = '1000'
+REDIS_LASE_HOST_PREFIX = 'lase.host.'
+REDIS_LASE_HOST_EXPIRE = '1000'
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
 
 class LaseRedisCache:
 
     def __init__(self):
-        self._r = redis.StrictRedis(host='localhost', port=6379, db=0)
+        self._r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
 
     def store_host(self, host, ports):
-        self._r.set(HOST_PREFIX + host, ports)
-        self._r.expire(HOST_PREFIX + host, HOST_EXPIRE)
+        self._r.set(REDIS_LASE_HOST_PREFIX + host, ports)
+        self._r.expire(REDIS_LASE_HOST_PREFIX + host, REDIS_LASE_HOST_EXPIRE)
 
     def load_host(self, host):
-        return self._r.get(HOST_PREFIX + host)
+        res = self._r.get(REDIS_LASE_HOST_PREFIX + host)
+        if not res:
+            return []
+        return res
