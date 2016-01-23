6140bfab2c0b999b96af8ac34b52e064f1103bf6
Filip Soltes <fillo@chrn.sk>
2016-01-23 13:53:50 +0100

    option for single host scan and crawl

--- lase.py
+++ lase.py
@@ -10,26 +10,33 @@ import src.api as api
 logger = logging.getLogger(__name__)
 
 
+def help():
+    print('lase.py [-s [host] | -c [host] | -a | --scan [host] | --crawl [host] | --api]')
+
 def main(argv):
     if not argv:
         print('please choose one of the options')
-        print('lase.py [-s | -c | -a | --scan | --crawl | --api]')
+        help()
 
     try:
-        opts, args = getopt.getopt(argv, "hsca", ["scan", "crawl", "api"])
+        opts, args = getopt.getopt(argv, "hsc:a", ["scan", "crawl", "api"])
     except getopt.GetoptError:
-        print('lase.py [-s | -c | -a | --scan | --crawl | --api]')
+        help()
         sys.exit(2)
 
     for opt, arg in opts:
         if opt == '-h':
-            print('lase.py [-s | -c | -a | --scan | --crawl | --api]')
+            help()
         elif opt in ("-a", "--api"):
             api.run_api()
-        elif opt in ("-c", "--crawl"):
+        elif opt in ("-c", "--crawl") and not args:
             producer.crawl_seq(config.ranges)
-        elif opt in ("-s", "--scan"):
+        elif opt in ("-c", "--crawl"):
+            producer.crawl_seq(args)
+        elif opt in ("-s", "--scan") and not args:
             producer.scan(config.ranges)
+        elif opt in ("-s", "--scan"):
+            producer.scan(args)
 
 
 if __name__ == '__main__':
