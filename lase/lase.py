f820a8f1f23d66420239b1990669fb506f03abf9
Filip Soltes <fillo@chrn.sk>
2016-01-25 20:56:24 +0100

    nicer help and cli options

--- lase/lase.py
+++ lase/lase.py
@@ -10,16 +10,28 @@ logger = logging.getLogger(__name__)
 
 
 def help():
-    print('lase.py [-s | -c | --scan | --crawl | --api | --crawl-host [host] | --scan-host [host] ]')
+    print('Usage: $LASE_HOME/env/bin/python lase.py [ -c | -C HOST | -s | -S HOST ]')
+    print('Try \'$LASE_HOME/env/bin/python lase.py --help\' for more information.')
+
+
+def help_long():
+    print('Usage: $LASE_HOME/env/bin/python lase.py [OPTION]')
+    print('LASE options:')
+    print('')
+    print('  -s, --scan                scans everything specified in config file')
+    print('  -S, --scan-host=HOST      scans specified HOST')
+    print('  -c, --crawl               scans and crawls everything specified in config file')
+    print('  -C, --crawl-host=HOST     scans and crawls specified HOST')
+    print('      --serve               starts default Flask Werkzeug web server; only for DEVELOPMENT')
+    print('      --help                display this help text and exit')
 
 
 def main(argv):
     if not argv:
-        print('please choose one of the options')
         help()
 
     try:
-        opts, args = getopt.getopt(argv, 'hsc', ['help', 'scan', 'crawl', 'crawl-host=', 'api'])
+        opts, args = getopt.getopt(argv, 'sS:cC:', ['help', 'scan', 'crawl', 'crawl-host=', 'scan-host=', 'serve'])
     except getopt.GetoptError:
         help()
         sys.exit(2)
@@ -27,16 +39,18 @@ def main(argv):
     for opt, arg in opts:
         if opt == '-h':
             help()
-        elif opt == '--api':
+        if opt == '--help':
+            help_long()
+        elif opt == '--serve':
             api.run_api()
         elif opt in ('-c', '--crawl'):
             producer.crawl_seq(config.ranges)
         elif opt in ('-s', '--scan'):
             producer.scan(config.ranges)
-        elif opt == '--crawl-host':
+        elif opt in ('-C', '--crawl-host'):
             producer.crawl_seq([arg])
-        elif opt == '--scan-host':
-            producer.scan(config.ranges)
+        elif opt in ('-S', '--scan-host'):
+            producer.scan([arg])
 
 
 if __name__ == '__main__':
